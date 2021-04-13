a93a16b0e505cdf4927e781f3a01a55b975ba9ec
Hossein Zare <zarehossein.hz1997@gmail.com>
2021-04-14 03:54:31 +0430

    Remove console.log().

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ function ReactNativeDropDownPicker({
         textStyle,
         isNull() ? placeholderStyle : containerLabelStyle,
     ]), [textStyle, value]);
-    console.log('rendering')
+
     return (
         <TouchableOpacity style={_containerStyle} onPress={onPressPicker}>
             <Text style={_containerLabelStyle}>
